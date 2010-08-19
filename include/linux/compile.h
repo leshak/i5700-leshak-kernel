82761168053752f8b277188a586b4ee57d2fb8dd
LeshaK <leshak@leshak.ru>
2010-08-19 11:54:41 +0400

    battery indicator by 5% steps

--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 19 */
+/* This file is auto generated, version 146 */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#19 Thu Jun 10 14:04:32 MSD 2010"
-#define LINUX_COMPILE_TIME "14:04:32"
+#define UTS_VERSION "#146 Thu Aug 19 11:24:38 MSD 2010"
+#define LINUX_COMPILE_TIME "11:24:38"
 #define LINUX_COMPILE_BY "leshak"
-#define LINUX_COMPILE_HOST "i5700-dev7"
+#define LINUX_COMPILE_HOST "leshak-udev7"
 #define LINUX_COMPILE_DOMAIN ""
-#define LINUX_COMPILER "gcc version 4.3.4 (crosstool-NG-1.6.1) "
+#define LINUX_COMPILER "gcc version 4.3.4 (crosstool-NG-1.8.0) "
