7ffdcae6a2dfd7596dd73f0603669dc70b53fb74
LeshaK <leshak@leshak.ru>
2010-08-19 11:56:31 +0400

    remove autoconf.h & compile.h

--- include/linux/compile.h
+++
@@ -1,8 +0,0 @@
-/* This file is auto generated, version 146 */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#146 Thu Aug 19 11:24:38 MSD 2010"
-#define LINUX_COMPILE_TIME "11:24:38"
-#define LINUX_COMPILE_BY "leshak"
-#define LINUX_COMPILE_HOST "leshak-udev7"
-#define LINUX_COMPILE_DOMAIN ""
-#define LINUX_COMPILER "gcc version 4.3.4 (crosstool-NG-1.8.0) "
